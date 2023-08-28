9ab7a6e7ca09d4634d00bdcb0961f153f2eecc69
Thecooljay <jamiugbemi@gmial.com>
2023-08-28 13:30:19 +0100

    Add keygen

---
+++ data_collection.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+name = input ("please enter your name:")
+age = input ( "please enter your age:")
+phone = input ("please enter your number:")
+email = input ( "please enter your email:")
+
+print (name)
+print(age)
+print(phone)
+print(email)
+
+
+#This will generate a random set of numbers as password ans print along with the user details
+
+
+
+
---
+++ keygendisp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import random
+name = input ("please enter your name:")
+age = input ( "please enter your age:")
+phone = input ("please enter your number:")
+email = input ( "please enter your email:")
+
+print (name)
+print(age)
+print(phone)
+print(email)
+
+
+#This will generate a random set of numbers as password and print along with the user details
+
+print("****************************************")
+print("****************************************")
+print("****                                    ")
+print("****     ****       ****                ")
+print("****     ****       ****                ")
+print("          **         **                 ")
+print("****************************************")
+
+user_number= random.randint(1,40)
+user_number= ([""])
+
+print(name, age, user_number)
